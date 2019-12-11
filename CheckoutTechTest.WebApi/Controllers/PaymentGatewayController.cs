b8a28a47464550c6806ca358cf563a2dd0a12064
Russell Day <russell.day.coder@gmail.com>
2019-12-11 21:06:56 +0000

    Removed default controller and added skeleton payment gateway

---
+++ CheckoutTechTest.WebApi/Controllers/PaymentGatewayController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CheckoutTechTest.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentGatewayController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public PaymentGatewayController(ILogger<PaymentGatewayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ObjectResult> Post(PaymentRequest payment)
+        {
+            return OkObjectResult(payment);
+        }
+    }
+}
---
+++ CheckoutTechTest.WebApi/Models/PaymentRequest.cs
@@ -0,0 +1,7 @@
+namespace CheckoutTechTest.WebApi.Models
+{
+    public class PaymentRequest
+    {
+        
+    }
+}
